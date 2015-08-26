aff08e0845d72565591c02461d1360728b0a611c
Lukas Wolfsteiner <coding@dotwee.de>
2015-08-26 18:15:00 +0200

    Fix useless checkBox for persistence

--- app/src/main/java/de/dotwee/micropinner/ui/EditActivity.java
+++ app/src/main/java/de/dotwee/micropinner/ui/EditActivity.java
@@ -6,14 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
-
+import android.widget.*;
 import de.dotwee.micropinner.R;
 import de.dotwee.micropinner.receiver.OnBootReceiver;
 import de.dotwee.micropinner.tools.PinHandler;
@@ -90,7 +83,7 @@ public class EditActivity extends AppCompatActivity implements View.OnClickListe
         else {
             pin.setTitle(newTitle);
             pin.setContent(newContent);
-
+            pin.setPersistent(checkBoxPersistentPin.isChecked());
             new PinHandler(this).persistPin(pin);
             finish();
         }

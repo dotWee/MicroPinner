18e31adcdbe88f6d9b954db88375423a5d387b8f
Lukas Wolfsteiner <coding@dotwee.de>
2015-08-26 18:31:21 +0200

    Simplify OnBootReceiver

--- app/src/main/java/de/dotwee/micropinner/receiver/OnBootReceiver.java
+++ app/src/main/java/de/dotwee/micropinner/receiver/OnBootReceiver.java
@@ -9,13 +9,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.util.Log;
-
-import java.util.Map;
-
 import de.dotwee.micropinner.R;
 import de.dotwee.micropinner.tools.PinHandler;
 import de.dotwee.micropinner.tools.PreferencesHandler;
 import de.dotwee.micropinner.ui.MainActivity;
+
+import java.util.Map;
 
 /**
  * Created by Lukas on 09.06.2015.
@@ -28,44 +27,67 @@ public class OnBootReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.i(LOG_TAG, "Received boot-intent.");
 
-
-        Intent mainIntent = new Intent(context, MainActivity.class);
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-        stackBuilder.addParentStack(MainActivity.class);
-        stackBuilder.addNextIntent(mainIntent);
-
-        PendingIntent mainPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // check if the should be shown
+        if (PreferencesHandler.getInstance(context).isShowNewPinEnabled())
+            notificationManager.notify(DEFAULT_NOTIFICATIONID, getNewPinNotification(context).build());
+
+        else notificationManager.cancel(DEFAULT_NOTIFICATIONID);
+
+        // check if pin-restoring is enabled
+        if (PreferencesHandler.getInstance(context).isRestoreEnabled()) {
+
+            // if yes, get a map of all saved pins
+            Map<Integer, PinHandler.Pin> pinMap = new PinHandler(context).getPins();
+
+            // foreach through them all
+            for (Map.Entry<Integer, PinHandler.Pin> entry : pinMap.entrySet()) {
+                PinHandler.Pin pin = entry.getValue();
+
+                PendingIntent pinIntent = pin.toIntent(context);
+
+                // create a notification out of the object
+                Notification pinNotification = pin.toNotification(context, pinIntent);
+
+                // and finally restore it
+                notificationManager.notify(pin.getId(), pinNotification);
+            }
+        }
+    }
+
+    private Notification.Builder getNewPinNotification(Context context) {
+
+        PendingIntent pendingIntent = getTaskStack(context, new Intent(context, MainActivity.class))
+                .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
         Notification.Builder defaultNotification = new Notification.Builder(context)
                 .setContentTitle(context.getResources().getString(R.string.main_name))
                 .setContentText(context.getResources().getString(R.string.message_pin_new))
                 .setPriority(Notification.PRIORITY_LOW)
                 .setOngoing(true)
                 .setAutoCancel(true)
-                .setContentIntent(mainPendingIntent)
+                .setContentIntent(pendingIntent)
                 .setSmallIcon(R.drawable.ic_pin_24dp);
 
         if (Build.VERSION.SDK_INT >= 17) defaultNotification.setShowWhen(false);
 
+        // the visibility api is only available on lollipop and up
         if (Build.VERSION.SDK_INT >= 21) {
             defaultNotification.setColor(context.getResources().getColor(R.color.accent));
             defaultNotification.setVisibility(Notification.VISIBILITY_PUBLIC);
         }
 
-        if (PreferencesHandler.getInstance(context).isShowNewPinEnabled())
-            notificationManager.notify(DEFAULT_NOTIFICATIONID, defaultNotification.build());
-        else notificationManager.cancel(DEFAULT_NOTIFICATIONID);
-
-        if (PreferencesHandler.getInstance(context).isRestoreEnabled()) {
-            Map<Integer, PinHandler.Pin> pinMap = new PinHandler(context).getPins();
-            for (Map.Entry<Integer, PinHandler.Pin> entry : pinMap.entrySet()) {
-                PinHandler.Pin pin = entry.getValue();
-
-                PendingIntent pinIntent = pin.toIntent(context);
-                Notification pinNotification = pin.toNotification(context, pinIntent);
-                notificationManager.notify(pin.getId(), pinNotification);
-            }
-        }
+        return defaultNotification;
     }
 
+    private TaskStackBuilder getTaskStack(Context context, Intent intent) {
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+
+        // use the main activity as parent
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(intent);
+
+        return stackBuilder;
+    }
 }

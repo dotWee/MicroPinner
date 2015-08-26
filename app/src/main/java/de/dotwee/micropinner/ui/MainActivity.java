d2653c3e9e10606a9f91b7d649ed6c92ca9733c3
Lukas Wolfsteiner <coding@dotwee.de>
2015-08-26 20:38:01 +0200

    No need to log every onClick view id

--- app/src/main/java/de/dotwee/micropinner/ui/MainActivity.java
+++ app/src/main/java/de/dotwee/micropinner/ui/MainActivity.java
@@ -170,7 +170,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Log.i(LOG_TAG, "clicked: " + v.getId());
 
         switch (v.getId()) {
             case R.id.buttonCancel:

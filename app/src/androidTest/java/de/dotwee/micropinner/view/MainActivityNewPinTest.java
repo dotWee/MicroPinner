996281142b154ca55c226fd8cfffb8613390dbae
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2016-07-25 13:24:12 +0200

    Add test to verify the whole persist-mechanism for user-created pins

--- app/src/androidTest/java/de/dotwee/micropinner/view/MainActivityNewPinTest.java
+++ app/src/androidTest/java/de/dotwee/micropinner/view/MainActivityNewPinTest.java
@@ -1,16 +1,22 @@
 package de.dotwee.micropinner.view;
 
+import android.app.Notification;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.Switch;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Map;
+
 import de.dotwee.micropinner.R;
+import de.dotwee.micropinner.tools.PinHandler;
 import de.dotwee.micropinner.tools.PreferencesHandler;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
@@ -20,10 +26,15 @@ import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isFocusable;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static de.dotwee.micropinner.tools.TestTools.getPreferencesHandler;
 import static de.dotwee.micropinner.tools.TestTools.recreateActivity;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -156,5 +167,72 @@ public class MainActivityNewPinTest {
 
         preferencesHandler.setAdvancedUse(true);
         assertTrue(preferencesHandler.isAdvancedUsed());
+    }
+
+    /**
+     * This method verifies the persist mechanism for user-created pins.
+     */
+    @Test
+    public void testUserCreateNewPin() throws Exception {
+        recreateActivity(activityTestRule);
+
+        PinHandler pinHandler = new PinHandler(activityTestRule.getActivity().getApplicationContext());
+        pinHandler.removeAllPins();
+
+        // enter a title
+        onView(withId(R.id.editTextTitle))
+                .perform(typeText(LOG_TAG));
+
+        // mark with high priority
+        onView(withId(R.id.spinnerPriority))
+                .perform(click());
+        String highPriority = activityTestRule.getActivity().getString(R.string.priority_high);
+        onData(allOf(is(instanceOf(String.class)), is(highPriority)))
+                .perform(click());
+        onView(withId(R.id.spinnerPriority))
+                .check(matches(withSpinnerText(R.string.priority_high)));
+
+        // mark with private visibility
+        onView(withId(R.id.spinnerVisibility))
+                .perform(click());
+        String privateVisibility = activityTestRule.getActivity().getString(R.string.visibility_private);
+        onData(allOf(is(instanceOf(String.class)), is(privateVisibility)))
+                .perform(click());
+        onView(withId(R.id.spinnerVisibility))
+                .check(matches(withSpinnerText(R.string.visibility_private)));
+
+        // expand layout
+        if (! ((Switch) activityTestRule.getActivity().findViewById(R.id.switchAdvanced)).isChecked()) {
+            onView(withId(R.id.switchAdvanced))
+                    .perform(click())
+                    .check(matches(isChecked()));
+        }
+
+        // mark as persistent
+        if (! activityTestRule.getActivity().isPersistent()) {
+            onView(withId(R.id.checkBoxPersistentPin))
+                    .perform(click())
+                    .check(matches(isChecked()));
+        }
+
+        // select persist button
+        onView(withId(R.id.buttonPin))
+                .perform(click());
+
+        // make sure pin exists
+        Map<Integer, PinHandler.Pin> pins = pinHandler.getPins();
+        for (Map.Entry<Integer, PinHandler.Pin> pinEntry : pins.entrySet()) {
+            String title = pinEntry.getValue().getTitle();
+            assertEquals(LOG_TAG, title);
+
+            int priority = pinEntry.getValue().getPriority();
+            assertEquals(Notification.PRIORITY_HIGH, priority);
+
+            int visibility = pinEntry.getValue().getVisibility();
+            assertEquals(Notification.VISIBILITY_PRIVATE, visibility);
+
+            boolean isPersistent = pinEntry.getValue().isPersistent();
+            assertTrue(isPersistent);
+        }
     }
 }

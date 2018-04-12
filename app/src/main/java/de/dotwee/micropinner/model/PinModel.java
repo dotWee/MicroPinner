d3e7d1c526b4f3caa8fa1e53241a191c085709d0
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-04-12 21:22:41 +0200

    Greenrobot's greenDAO as dependency defined

---
+++ app/src/main/java/de/dotwee/micropinner/model/PinModel.java
@@ -0,0 +1,23 @@
+package de.dotwee.micropinner.model;
+
+/**
+ * Created by lukas on 12.04.18.
+ */
+
+public class PinModel {
+
+    // notification id!
+    private long id;
+
+    private String title;
+    private String content;
+
+    private int visibility;
+    private int priority;
+    private int notification;
+
+    private boolean persistent;
+    private boolean showActions;
+
+
+}

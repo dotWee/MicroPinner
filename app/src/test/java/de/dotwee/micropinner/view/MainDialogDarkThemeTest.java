0d27ebb79acd93705efb81933d6e4aaad4223927
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-05-01 16:43:52 +0200

    Simplify theme tests using config qualifies

--- app/src/test/java/de/dotwee/micropinner/view/MainDialogDarkThemeTest.java
+++ app/src/test/java/de/dotwee/micropinner/view/MainDialogDarkThemeTest.java
@@ -3,7 +3,6 @@ package de.dotwee.micropinner.view;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,16 +12,16 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.android.controller.ActivityController;
+import org.robolectric.annotation.Config;
 
 import de.dotwee.micropinner.R;
 
-import static de.dotwee.micropinner.tools.ThemeTools.changeUiMode;
 import static de.dotwee.micropinner.tools.ThemeTools.getAccentColor;
 import static de.dotwee.micropinner.tools.ThemeTools.getBackgroundColor;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(RobolectricTestRunner.class)
-public class MainDialogTest {
+public class MainDialogDarkThemeTest {
     ActivityController<MainDialog> mainDialogActivityController;
 
     @Before
@@ -35,27 +34,27 @@ public class MainDialogTest {
      */
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Test
+    @Config(qualifiers = "night")
     public void testThemeLightCaptionAccent() {
-        changeUiMode(mainDialogActivityController, AppCompatDelegate.MODE_NIGHT_NO);
         MainDialog mainDialog = mainDialogActivityController.get();
 
         TextView textViewCaptionTitle = mainDialog.findViewById(R.id.textViewCaptionTitle);
-        int colorCaptionTitleExpected = getAccentColor(mainDialog, true);
+        int colorCaptionTitleExpected = getAccentColor(mainDialog, false);
         int colorCaptionTitleActual = textViewCaptionTitle.getCurrentTextColor();
         assertEquals(colorCaptionTitleExpected, colorCaptionTitleActual);
 
         TextView textViewCaptionContent = mainDialog.findViewById(R.id.textViewCaptionContent);
-        int colorCaptionContentExpected = getAccentColor(mainDialog, true);
+        int colorCaptionContentExpected = getAccentColor(mainDialog, false);
         int colorCaptionContentActual = textViewCaptionContent.getCurrentTextColor();
         assertEquals(colorCaptionContentExpected, colorCaptionContentActual);
 
         TextView textViewCaptionPriority = mainDialog.findViewById(R.id.textViewCaptionPriority);
-        int colorCaptionPriorityExpected = getAccentColor(mainDialog, true);
+        int colorCaptionPriorityExpected = getAccentColor(mainDialog, false);
         int colorCaptionPriorityActual = textViewCaptionPriority.getCurrentTextColor();
         assertEquals(colorCaptionPriorityExpected, colorCaptionPriorityActual);
 
         TextView textViewCaptionVisibility = mainDialog.findViewById(R.id.textViewCaptionVisibility);
-        int colorCaptionVisibilityExpected = getAccentColor(mainDialog, true);
+        int colorCaptionVisibilityExpected = getAccentColor(mainDialog, false);
         int colorCaptionVisibilityActual = textViewCaptionVisibility.getCurrentTextColor();
         assertEquals(colorCaptionVisibilityExpected, colorCaptionVisibilityActual);
     }
@@ -65,12 +64,12 @@ public class MainDialogTest {
      */
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Test
+    @Config(qualifiers = "night")
     public void testThemeLightBackground() throws Exception {
-        changeUiMode(mainDialogActivityController, AppCompatDelegate.MODE_NIGHT_NO);
         MainDialog mainDialog = mainDialogActivityController.get();
 
         View view = mainDialog.findViewById(android.R.id.content);
-        int expectedColor = getBackgroundColor(mainDialogActivityController.get(), true);
+        int expectedColor = getBackgroundColor(mainDialogActivityController.get(), false);
         int actualColor = ((ColorDrawable) view.getBackground()).getColor();
         assertEquals(expectedColor, actualColor);
     }
